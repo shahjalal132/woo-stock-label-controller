a11a1c48486d12e9846eb96035864f361dd60cca
Shah Jalal <rjshahjalal132@gmail.com>
2025-04-29 10:37:59 +0600

    enqueue assets and add files

--- woo-stock-text-enhance.php
+++ woo-stock-text-enhance.php
@@ -15,6 +15,14 @@
 
 defined( "ABSPATH" ) || exit( "Direct Access Not Allowed" );
 
+if ( !defined( 'PLUGIN_DIR_BASE_PATH' ) ) {
+    define( 'PLUGIN_DIR_BASE_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+if ( !defined( 'PLUGIN_DIR_BASE_URL' ) ) {
+    define( 'PLUGIN_DIR_BASE_URL', plugin_dir_url( __FILE__ ) );
+}
+
 add_action( 'woocommerce_before_add_to_cart_form', 'custom_display_stock_status', 20 );
 function custom_display_stock_status() {
     global $product;
@@ -52,3 +60,21 @@ function custom_woocommerce_get_availability_text( $availability, $product ) {
 
     return $availability;
 }
+
+// enqueue assets
+function wste_enqueue_assets( $page_now ) {
+
+    // file_put_contents( PLUGIN_DIR_BASE_PATH . 'logs/logs', $page_now );
+
+    if ( 'post.php' == $page_now ) {
+        // enqueue admin js
+        wp_enqueue_script( "admin-script", PLUGIN_DIR_BASE_URL . "assets/admin/js/admin-scripts.js", [ 'jquery' ], time(), true );
+        // localize script to pass ajax url
+        wp_localize_script( 'admin-script', 'wste_ajax', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+    }
+}
+add_action( 'admin_enqueue_scripts', 'wste_enqueue_assets' );
+
+
+// require metabox file.
+require_once PLUGIN_DIR_BASE_PATH . 'includes/class-wste-metabox.php';
